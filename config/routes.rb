0d8dd938d64048d8576e83320a8d5e74d5aaa848
Tungdt <tung189cv@gmail.com>
2022-01-17 16:13:19 +0700

    footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index", index, create, new, edit, update, show, delete
+  resources :comics, only: [:index]
 end
